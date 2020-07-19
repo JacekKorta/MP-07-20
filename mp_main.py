4fe8bfbf863cde2435adb8f372339ad36bb666fb
JacekKorta <jacek.korta@gmail.com>
2020-07-19 21:20:26 +0200

    adds full tests

---
+++ mp_main.py
@@ -0,0 +1,85 @@
+from typing import Tuple
+
+
+def input_data_validator(input_data):
+    if isinstance(input_data, int) and 1 <= input_data <= 100:
+        return True
+    else:
+        return False
+
+
+def count_large_boxes(quantity: int) -> Tuple[int, int]:
+    large = quantity // 9
+    rest = quantity % 9
+    if rest > 6:
+        large += 1
+        rest = 0
+    return large, rest
+
+
+def count_medium_boxes(rest_from_large: int) -> int:
+    if rest_from_large:
+        medium = rest_from_large // 6
+        rest = rest_from_large % 6
+        if rest > 3:
+            medium += 1
+            rest = 0
+        return medium
+    else:
+        return 0
+
+
+def count_small_boxes(rest_from_medium: int) -> int:
+    if rest_from_medium:
+        small = rest_from_medium // 3
+        rest = rest_from_medium % 3
+        if rest:
+            small += 1
+        return small
+    else:
+        return 0
+
+
+def count_bulk_boxes(boxes: dict) -> int:
+    all_boxes = sum(boxes.values())
+    bulk = all_boxes // 3
+    if all_boxes % 3 > 0 and all_boxes > 1:
+        bulk += 1
+    return bulk
+
+
+def check_is_it_exception(quantity: int) -> bool:
+    if 9 < quantity < 16:
+        return True
+    return False
+
+
+def customer_exception(quantity: int) -> dict:
+    if quantity < 13:
+        return {'small': 0, 'medium': 2, 'large': 0, 'bulk': 1}
+    else:
+        return {'small': 0, 'medium': 0, 'large': 2, 'bulk': 1}
+
+
+def main(quantity: int):
+    if input_data_validator(quantity):
+        if check_is_it_exception(quantity):
+            boxes = customer_exception(quantity)
+        else:
+            boxes = {'small': 0, 'medium': 0, 'large': 0, 'bulk': 0}
+            boxes['large'], rest = count_large_boxes(quantity)
+            if rest > 3:
+                boxes['medium'] = count_medium_boxes(rest)
+            else:
+                boxes['small'] = count_small_boxes(rest)
+            boxes['bulk'] = count_bulk_boxes(boxes)
+        return boxes
+    else:
+        return 'Input data should be an integer in the range 1-100'
+
+
+
+
+
+
+
--- tests.py
+++ tests.py
@@ -1,19 +1,126 @@
 import unittest
 
-from main import fix_the_boxes
+from mp_main import main
 
 
-class TestMainTask(unittest.TestCase):
-
+class TestDataValidation(unittest.TestCase):
+    """Test data validation. The Input data should be an integer in the range 1-100"""
     def test_data_validation_input_is_string(self):
-        self.assertEqual(fix_the_boxes('test string'), 'Input data should be an integer in the range 1-100')
+        self.assertEqual(main('test string'), 'Input data should be an integer in the range 1-100')
 
     def test_data_validation_input_is_out_of_range(self):
-        self.assertEqual(fix_the_boxes(101), 'Input data should be an integer in the range 1-100')
-        self.assertEqual(fix_the_boxes(0), 'Input data should be an integer in the range 1-100')
+        self.assertEqual(main(101), 'Input data should be an integer in the range 1-100')
+        self.assertEqual(main(0), 'Input data should be an integer in the range 1-100')
 
     def test_data_validation_input_is_negative(self):
-        self.assertEqual(fix_the_boxes(-1), 'Input data should be an integer in the range 1-100')
+        self.assertEqual(main(-1), 'Input data should be an integer in the range 1-100')
+
+
+class TestDataFromTable(unittest.TestCase):
+    """Tests the data from the source table (pdf)"""
+    def test_data_is_1_expect_1s(self):
+        self.assertEqual(main(1), {'small': 1, 'medium': 0, 'large': 0, 'bulk': 0})
+
+    def test_data_is_2_expect_1s(self):
+        self.assertEqual(main(2), {'small': 1, 'medium': 0, 'large': 0, 'bulk': 0})
+
+    def test_data_is_3_expect_1s(self):
+        self.assertEqual(main(3), {'small': 1, 'medium': 0, 'large': 0, 'bulk': 0})
+
+    def test_data_is_4_expect_1m(self):
+        self.assertEqual(main(4), {'small': 0, 'medium': 1, 'large': 0, 'bulk': 0})
+
+    def test_data_is_5_expect_1m(self):
+        self.assertEqual(main(5), {'small': 0, 'medium': 1, 'large': 0, 'bulk': 0})
+
+    def test_data_is_6_expect_1m(self):
+        self.assertEqual(main(6), {'small': 0, 'medium': 1, 'large': 0, 'bulk': 0})
+
+    def test_data_is_7_expect_1l(self):
+        self.assertEqual(main(7), {'small': 0, 'medium': 0, 'large': 1, 'bulk': 0})
+
+    def test_data_is_8_expect_1l(self):
+        self.assertEqual(main(8), {'small': 0, 'medium': 0, 'large': 1, 'bulk': 0})
+
+    def test_data_is_9_expect_1l(self):
+        self.assertEqual(main(9), {'small': 0, 'medium': 0, 'large': 1, 'bulk': 0})
+
+    def test_data_is_10_expect_2m_1b(self):
+        self.assertEqual(main(10), {'small': 0, 'medium': 2, 'large': 0, 'bulk': 1})
+
+    def test_data_is_11_expect_2m_1b(self):
+        self.assertEqual(main(11), {'small': 0, 'medium': 2, 'large': 0, 'bulk': 1})
+
+    def test_data_is_12_expect_2m_1b(self):
+        self.assertEqual(main(10), {'small': 0, 'medium': 2, 'large': 0, 'bulk': 1})
+
+    def test_data_is_13_expect_2l_1b(self):
+        self.assertEqual(main(13), {'small': 0, 'medium': 0, 'large': 2, 'bulk': 1})
+
+    def test_data_is_14_expect_2l_1b(self):
+        self.assertEqual(main(14), {'small': 0, 'medium': 0, 'large': 2, 'bulk': 1})
+
+    def test_data_is_15_expect_2l_1b(self):
+        self.assertEqual(main(15), {'small': 0, 'medium': 0, 'large': 2, 'bulk': 1})
+
+    def test_data_is_16_expect_2l_1b(self):
+        self.assertEqual(main(16), {'small': 0, 'medium': 0, 'large': 2, 'bulk': 1})
+
+    def test_data_is_17_expect_2l_1b(self):
+        self.assertEqual(main(17), {'small': 0, 'medium': 0, 'large': 2, 'bulk': 1})
+
+    def test_data_is_19_expect_1s_2l_1b(self):
+        self.assertEqual(main(19), {'small': 1, 'medium': 0, 'large': 2, 'bulk': 1})
+
+    def test_data_is_21_expect_1s_2l_1b(self):
+        self.assertEqual(main(21), {'small': 1, 'medium': 0, 'large': 2, 'bulk': 1})
+
+    def test_data_is_22_expect_1m_2l_1b(self):
+        self.assertEqual(main(22), {'small': 0, 'medium': 1, 'large': 2, 'bulk': 1})
+
+    def test_data_is_24_expect_1m_2l_1b(self):
+        self.assertEqual(main(24), {'small': 0, 'medium': 1, 'large': 2, 'bulk': 1})
+
+    def test_data_is_25_expect_3l_1b(self):
+        self.assertEqual(main(25), {'small': 0, 'medium': 0, 'large': 3, 'bulk': 1})
+
+    def test_data_is_26_expect_3l_1b(self):
+        self.assertEqual(main(26), {'small': 0, 'medium': 0, 'large': 3, 'bulk': 1})
+
+    def test_data_is_27_expect_1m_2l_1b(self):
+        self.assertEqual(main(27), {'small': 0, 'medium': 0, 'large': 3, 'bulk': 1})
+
+    def test_data_is_28_expect_1s_3l_2b(self):
+        self.assertEqual(main(28), {'small': 1, 'medium': 0, 'large': 3, 'bulk': 2})
+
+    def test_data_is_30_expect_1s_3l_2b(self):
+        self.assertEqual(main(30), {'small': 1, 'medium': 0, 'large': 3, 'bulk': 2})
+
+    def test_data_is_31_expect_1m_3l_2b(self):
+        self.assertEqual(main(31), {'small': 0, 'medium': 1, 'large': 3, 'bulk': 2})
+
+    def test_data_is_33_expect_1m_3l_2b(self):
+        self.assertEqual(main(33), {'small': 0, 'medium': 1, 'large': 3, 'bulk': 2})
+
+    def test_data_is_34_expect_4l_2b(self):
+        self.assertEqual(main(34), {'small': 0, 'medium': 0, 'large': 4, 'bulk': 2})
+
+    def test_data_is_36_expect_4l_2b(self):
+        self.assertEqual(main(36), {'small': 0, 'medium': 0, 'large': 4, 'bulk': 2})
+
+    def test_data_is_99_expect_11l_4b(self):
+        self.assertEqual(main(99), {'small': 0, 'medium': 0, 'large': 11, 'bulk': 4})
+
+    def test_data_is_100_expect_1s_11l_4b(self):
+        self.assertEqual(main(100), {'small': 1, 'medium': 0, 'large': 11, 'bulk': 4})
+
+
+class TestItemsCover(unittest.TestCase):
+    """Tests that all items are in the box"""
+    def test_items_cover(self):
+        for qty in range(1, 101):
+            boxes = main(qty)
+            self.assertLessEqual(qty, (boxes['small']*3 + boxes['medium']*6 + boxes['large']*9))
 
 
 if __name__ == '__main__':
